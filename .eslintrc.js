3c61c24302f0b06a9f31caa616bac8ac6a4fe248
K.K <gokennsirou@yahoo.co.jp>
2021-04-12 00:22:33 +0900

    eslintの設定を修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,8 @@ module.exports = {
   //   // sourceType: 'module'
   // },
   plugins: ['@typescript-eslint'],
-  rules: {},
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  },
   ignorePatterns: ['/dist/**']
 }
